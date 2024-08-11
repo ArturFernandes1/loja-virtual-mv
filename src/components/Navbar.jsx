cdec146fd1c957527219737b00e6afe0dbc83ea5
Artur Fernandes <141247421+ArturFernandes1@users.noreply.github.com>
2024-08-11 19:07:51 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css'; 
-import logo from '../Assets/logo.svg'
+import logoempresa from '../Assets/logoempresa.svg'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +13,7 @@ const Navbar = () => {
         <header>
             <div className={`topnav${isOpen ? ' responsive' : ''}`} id="myTopnav">
                 <img 
-                 className="logo-empresa" src={logo} alt="lodo da empresa" />
+                 className="logo-empresa" src={logoempresa} alt="lodo da empresa" />
                 <a className='item-1' href="/">HOME</a>
                 <a className='item-2' href="/Empresa">EMPRESA</a>
                 <a className='item-3' href="/Contato">CONTATO</a>
